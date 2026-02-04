2475ad8be4f9af5faa367a00aa5b2fa9af402164
Nova <64618129+x1325990526@users.noreply.github.com>
2026-02-04 14:45:22 +0800

    Complete 06-repeatStr
    const string can't change,thus using "let"

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
+//    expect(repeatString('hey', 3)).toEqual('heyheyhey');
+const repeatString = function(str,n) {
+
+    if(n<0)return "ERROR";
+    let finalStr = '';
+    for(i=0;i<n;i++){
+        finalStr += str;
+    } 
+
+    return finalStr;
+
 
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
