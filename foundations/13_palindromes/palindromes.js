c5c424a568a5baba16ef93f6c9b2be6f969eb493
Nova <64618129+x1325990526@users.noreply.github.com>
2026-04-16 17:25:32 +0800

    feat: solve 13 with rex

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+/**
+ * @param {string} str
+ */
 
+
+const palindromes = function (str) {
+    let arrFilter = str.split("").map(char => char.toLocaleLowerCase())
+                                .filter(char => /[a-zA-Z0-9]/.test(char));
+
+
+    let isPalindromes = true;
+    for (let i = 0; i < arrFilter.length; i++) {
+
+        if(arrFilter[i] !== arrFilter.toReversed()[i]) isPalindromes =false;
+        
+    }                             
+    return isPalindromes;                         
+
+    
 };
 
 // Do not edit below this line
