37cd148b6efe0ba7ab01f62a2217732c52091967
Nova <64618129+x1325990526@users.noreply.github.com>
2026-04-19 11:23:47 +0800

    solve 15

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
 
+/**
+ * @param {[]} books
+ */
+const getTheTitles = function(books) {
+    const arr = [];
+    for (const book of books) {
+        arr.push(book.title);
+    }
+    return arr;
 };
 
 // Do not edit below this line
