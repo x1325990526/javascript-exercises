991f1ca999acd70d75262dde7390b3e115fd85fb
Nova <64618129+x1325990526@users.noreply.github.com>
2026-04-19 10:48:08 +0800

    feat: complete exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
 
+    if(x == 0) return 0;
+    if(x <= 0) return "OOPS";
+
+    arr = [1,1];
+    if(x < 3) return arr[x-1];
+    
+    let count = x -2;
+
+    while(count > 0){
+        const tmp = arr[1];
+        arr[1] = arr[0] + arr[1];
+        arr[0] = tmp;
+        count--;
+    }
+    return arr[1];
 };
 
 // Do not edit below this line
