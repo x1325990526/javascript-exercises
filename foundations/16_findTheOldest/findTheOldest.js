21fbe8bb28d314dbc76ee80e42d68c9dd068df7e
Nova <64618129+x1325990526@users.noreply.github.com>
2026-04-20 16:31:11 +0800

    feat: solve 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,60 @@
-const findTheOldest = function() {
+/**
+ * 
+ * @param {[]} people 
+ */
+
+const findTheOldest = function(people) {
+    const old = people.reduce((oldest,person) =>{
+        let curYear = 0;
+        let oldYear = 0;
+
+        if(oldest.yearOfDeath == undefined){
+            oldYear = new Date().getFullYear() - oldest.yearOfBirth;
+        }else{
+            oldYear = oldest.yearOfDeath - oldest.yearOfBirth;
+        }
+        
+        if(person.yearOfDeath == undefined){
+            curYear = new Date().getFullYear() - person.yearOfBirth;
+        }else{
+            curYear = person.yearOfDeath - person.yearOfBirth;
+        }
+
+        return curYear > oldYear ? person : oldest;
+
+    },{
+        name: "",
+        yearOfBirth: 1900,
+        yearOfDeath: 1900,
+      });
+
+      return old;
+
+      
 
 };
+
+
+/**
+ *   const people = [
+      {
+        name: "Carly",
+        yearOfBirth: 1942,
+        yearOfDeath: 1970,
+      },
+      {
+        name: "Ray",
+        yearOfBirth: 1962,
+        yearOfDeath: 2011,
+      },
+      {
+        name: "Jane",
+        yearOfBirth: 1912,
+        yearOfDeath: 1941,
+      },
+    ]
+ * 
+ */
 
 // Do not edit below this line
 module.exports = findTheOldest;
